591275a7231c1d1f0b9aacacccf8938c39d782e1
Rishabh <joshir838@gmail.com>
2025-01-30 21:29:55 +0530

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -15,44 +15,43 @@ function App() {
           <h2>My Location</h2>
 
           <div className="mylocation_card">
-            <div className="mylocation_data">
-              <div className="mylocation_heading">
-                <h2>
-                  Noida, <span>Uttar Pradesh</span>
+            <div className="mylocation_data weather_card">
+              <div className="mylocation_name">
+                <h2 className="mylocation_heading">
+                  Noida,{" "}
+                  <span className="location_heading_state">Uttar Pradesh</span>
                 </h2>
-                <span>IN</span>
+                <span className="location_heading_country">IN</span>
               </div>
 
               <div className="current_weather">
                 <p className="cur_temp">20&deg;</p>
-                <div className="min_max">
+                <div className="min_max current_weather_detail_card">
                   <p>Max: 22</p>
                   <p>Min: 10</p>
                 </div>
-                <div className="logo">
+                <div className="logo ">
                   <span>☀️</span>
                   <p>Clear Sky</p>
                 </div>
 
-                <div className="current_weather_cards">
+                <div className="humidity current_weather_detail_card">
                   <span>💧</span>
                   <p>
-                    Humidity
-                    <span>69%</span>
+                    Humidity <span>69%</span>
                   </p>
                 </div>
 
-                <div className="current_weather_cards">
+                <div className="wind_speed current_weather_detail_card">
                   <span>💨</span>
                   <p>
-                    Wind Speed
-                    <span>69%</span>
+                    Wind Speed <span>69%</span>
                   </p>
                 </div>
               </div>
             </div>
 
-            <div className="mylocation_weather"></div>
+            <div className="mylocation_weather weather_card">daily Weather</div>
           </div>
         </section>
       </main>
