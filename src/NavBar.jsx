1c6ef1890fd2fa318a4f8c273a087ba14bf5bb46
Rishabh <joshir838@gmail.com>
2025-02-21 11:36:03 +0530

    add api key

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,15 +1,27 @@
-export function NavBar() {
+// eslint-disable-next-line react/prop-types
+export function NavBar({ query, setQuery }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <nav className="navBar">
       <h2>Weather</h2>
-      <div>
+      <form onSubmit={handleSubmit}>
         <input
           className="search"
           name="search"
           type="text"
           placeholder="Search..."
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
         />
-      </div>
+        <button className="search-btn" type="submit">
+          Search
+        </button>
+      </form>
     </nav>
   );
 }
