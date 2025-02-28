111264031ff5ad99af2f0faeb6301323766de844
Rishabh <joshir838@gmail.com>
2025-02-28 16:05:09 +0530

    error reset and svg added

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ function App() {
               longitude
             );
 
+            setError("");
             setWeatherData(weatherData);
             setAddCity({
               id: weatherData.data.id,
@@ -83,6 +84,7 @@ function App() {
             weatherData.locationData[0].lon
           );
 
+          setError("");
           setWeatherData(weatherData);
           setAddCity({
             id: weatherData.data.id,
@@ -119,6 +121,7 @@ function App() {
     async function fetchWeather() {
       try {
         if (!selectedCity) return;
+
         const weatherData = await fetchCurrentWeather(
           selectedCity.lat,
           selectedCity.lon
@@ -129,6 +132,7 @@ function App() {
           selectedCity.lon
         );
 
+        setError("");
         setWeatherData(weatherData);
         setAddCity({
           id: weatherData.data.id,
@@ -251,7 +255,13 @@ function CurrentWeather({ weatherData, handleAddToFavourites, addCity }) {
         title="Add to Favourites"
         onClick={() => handleAddToFavourites(addCity)}
       >
-        ❤️
+        <svg
+          viewBox="0 0 24 24"
+          fill="#353535"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+        </svg>
       </button>
     </section>
   );
